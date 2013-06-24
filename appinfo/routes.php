c14893ae6e3b3ff0d4adb6a52a8f39eb04764df0
Andrei Duma <andrei.duma.dorian@gmail.com>
2013-06-24 09:57:19 +0300

    Added a /routes route

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -8,8 +8,14 @@ use \OCA\Marble\DependencyInjection\DIContainer;
 
 
 $this->create('marble_index', '/')->get()->action(
-    function($params){
+    function($params) {
         // call the index method on the class PageController
         App::main('PageController', 'index', $params, new DIContainer());
+    }
+);
+
+$this->create('marble_routes_get_all', '/routes')->get()->action(
+    function($params) {
+        App::main('RouteController', 'getAll', $params, new DIContainer());
     }
 );
