59be2e7faab54cc082cd342bd20b3c3ac43d2e90
Andrei Duma <andrei.duma.dorian@gmail.com>
2013-06-25 15:28:18 +0300

    The Route class now extends Entity

--- db/route.php
+++ db/route.php
@@ -2,111 +2,27 @@
 
 namespace OCA\Marble\Db;
 
-class Route {
+use \OCA\AppFramework\Db\Entity;
 
-    //private $id;
-    private $userId;
-    private $timestamp;
-    //private $kml;
-    private $name;
-    private $duration;
-    private $distance;
+class Route extends Entity {
 
-    public function __construct($row=null) {
-        if ($row) {
-            $this->fromRow($row);
-        }
+    public $userId;
+    public $timestamp;
+    public $name;
+    public $duration;
+    public $distance;
+
+    public function __construct() {
+        $this->addType('duration', 'int');
+        $this->addType('distance', 'int');
     }
 
-    public function fromRow($row) {
-        //$this->id        = $row['id'];
-        $this->userId    = $row['user_id'];
-        $this->timestamp = $row['timestamp'];
-        //$this->kml       = $row['kml'];
-        $this->name      = $row['name'];
-        $this->duration  = $row['duration'];
-        $this->distance  = $row['distance'];
-    }
-
-    public function toArray() {
+    public function toAPI() {
         return array(
-            'user_id'   => $this->userId,
-            'timestamp' => $this->timestamp,
-            'name'      => $this->name,
-            'duration'  => $this->duration,
-            'distance'  => $this->distance
+            'timestamp' => $this->getTimestamp(),
+            'name' => $this->getName(),
+            'duration' => $this->getDuration(),
+            'distance' => $this->getDistance()
         );
     }
-
-    /**
-     * GETTERS
-     */
-
-/*
-    public function getId() {
-        return $this->id;
-    }
-*/
-
-    public function getUserId() {
-        return $this->userId;
-    }
-
-    public function getTimestamp() {
-        return $this->timestamp;
-    }
-
-/*
-    public function getKml() {
-        return $this->kml;
-    }
-*/
-
-    public function getName() {
-        return $this->name;
-    }
-
-    public function getDuration() {
-        return $this->duration;
-    }
-
-    public function getDistance() {
-        return $this->distance;
-    }
-
-    /**
-     * SETTERS
-     */
-/*
-    public function setId($id) {
-        $this->id = $id;
-    }
-*/
-
-    public function setUserId($userId) {
-        $this->userId = $userId;        
-    }
-
-    public function setTimestamp($timestamp) {
-        $this->timestamp = $timestamp;
-    }
-
-/*
-    public function setKml($kml) {
-        $this->kml = $kml;
-    }
-*/
-
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    public function setDuration($duration) {
-        $this->duration = $duration;
-    }
-
-    public function setDistance($distance) {
-        $this->distance = $distance;
-    }
-
 }
