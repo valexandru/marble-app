78151c479eea3e01e7f00a51dc9ba01d3b2c949b
Andrei Duma <andrei.duma.dorian@gmail.com>
2013-06-27 10:40:30 +0300

    BusinessLayer now contains an instance of API

--- businesslayer/businesslayer.php
+++ businesslayer/businesslayer.php
@@ -4,9 +4,11 @@ namespace OCA\Marble\BusinessLayer;
 
 class BusinessLayer {
 
+    protected $api;
     protected $mapper;
 
-    public function __construct($mapper) {
+    public function __construct($api, $mapper) {
+        $this->api = $api;
         $this->mapper = $mapper;
     }
 
