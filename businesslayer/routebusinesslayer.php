d84ff65940f399bedcc9a5663b621f8758aa4363
Andrei Duma <andrei.duma.dorian@gmail.com>
2013-06-27 10:41:17 +0300

    Added a  method to the RouteBusinessLayer

--- businesslayer/routebusinesslayer.php
+++ businesslayer/routebusinesslayer.php
@@ -2,24 +2,35 @@
 
 namespace OCA\Marble\BusinessLayer;
 
+use \OCA\Marble\Db\Route;
 use \OCA\Marble\Db\RouteMapper;
 
 class RouteBusinessLayer extends BusinessLayer {
 
     public function __construct($api) {
-        parent::__construct(new RouteMapper($api));
+        parent::__construct($api, new RouteMapper($api));
     }
 
     public function findAll($userId) {
         $routes = $this->mapper->findAll($userId);
 
-        // Take out the userId fields
+        // Keep only required fields, using toAPI()
         $finalRoutes = array();
         foreach ($routes as $route) {
             array_push($finalRoutes, $route->toAPI());
         }
 
         return $finalRoutes;
+    }
+
+    public function create($params) {
+        $params['userId'] = $this->api->getUserId();
+
+        // Build the Route
+        $route = Route::fromParams($params);
+
+        // Insert it
+        $this->mapper->insert($route);
     }
 
 }
