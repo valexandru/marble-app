c30400563b959152e9470255cb30a7b8920729cc
Andrei Duma <andrei.duma.dorian@gmail.com>
2013-07-04 11:38:38 +0300

    Fixed paths and enhanced error messages

--- filemanager/filemanager.php
+++ filemanager/filemanager.php
@@ -9,25 +9,25 @@ class FileManager {
     public static function read($userId, $path) {
         $view = new View('');
 
-        $contents = $view->file_get_contents($userId . '/' . $path);
+        $contents = $view->file_get_contents($userId . '/marble/' . $path);
         if (!$contents)
-            throw new FileManagerException('Could not read from file.');
+            throw new FileManagerException('Could not read from file \'' . $path . '\'');
 
         return $contents;
     }
 
     public static function write($userId, $path, $contents) {
         $view = new View('');
-        if (!$r = $view->file_put_contents($userId . '/' . $path, $contents))
-            throw new FileManagerException('Could not write to file.');
+        if (!$r = $view->file_put_contents($userId . '/marble/' . $path, $contents))
+            throw new FileManagerException('Could not write to file \'' . $path . '\'');
 
         return $r;
     }
 
     public static function delete($userId, $path) {
         $view = new View('');
-        if (!$r = $view->unlink($userId . '/' . $path))
-            throw new FileManagerException('Could not delete the file.');
+        if (!$r = $view->unlink($userId . '/marble/' . $path))
+            throw new FileManagerException('Could not delete file \'' . $path . '\'');
 
         return $r;
     }
