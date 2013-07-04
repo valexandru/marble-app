7bb8bf97095f6e0889bc0b110bdddb2f3b33cf10
Andrei Duma <andrei.duma.dorian@gmail.com>
2013-07-04 10:01:34 +0300

    Extended FileManager to RouteManager for handling routes i/o

---
+++ filemanager/routemanager.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace \OCA\Marble\FileManager;
+
+class RouteManager extends FileManager {
+
+    public static function read($userId, $timestamp) {
+        return parent::read($userId, 'routes/' . $timestamp);
+    }
+
+    public static function write($userId, $timestamp, $kml) {
+        return parent::write($userId, 'routes/' . $timestamp, $kml);
+    }
+
+    public static function delete($userId, $timestamp) {
+        return parent::delete($userId, 'routes/' . $timestamp);
+    }
+
+}
