225231af5d1838219e0644c51bd981e417dab9ec
Andrei Duma <andrei.duma.dorian@gmail.com>
2013-07-06 17:57:13 +0300

    Implemented get & update methods in the BookmarksAPI controller

--- external/bookmarksapi.php
+++ external/bookmarksapi.php
@@ -1,9 +1,14 @@
 <?php
 
-namespace OCA\Marble\BookmarksAPI;
+namespace OCA\Marble\External;
 
 use \OCA\AppFramework\Controller\Controller;
+use \OCA\AppFramework\Http\Http;
+use \OCA\AppFramework\Http\JSONResponse;
+use \OCA\AppFramework\Http\TextResponse;
 
+use \OCA\Marble\BusinessLayer\BookmarksBusinessLayer;
+use \OCA\Marble\BusinessLayer\BusinessLayerException;
 
 class BookmarksAPI extends Controller {
 
@@ -19,7 +24,22 @@ class BookmarksAPI extends Controller {
      * @API
      */
     public function get() {
-        //
+        $layer = new BookmarksBusinessLayer($this->api);
+
+        $userId = $this->api->getUserId();
+
+        try {
+            $kml = $layer->getKML($userId);
+
+            return new TextResponse($kml);
+        } catch (BusinessLayerException $e) {
+            return new JSONResponse(array(
+                "status" => "error",
+                "message" => $e->getMessage()
+            ), Http::STATUS_NOT_FOUND);
+        }
+
+        return new TextResponse($kml);
     }
 
     /**
@@ -30,7 +50,25 @@ class BookmarksAPI extends Controller {
      * @API
      */
     public function update() {
-        //
+        $layer = new BookmarksBusinessLayer($this->api);
+
+        $bms_file = $this->getUploadedFile('bookmarks');
+
+        $bms = file_get_contents($bms_file['tmp_name']);
+        $userId = $this->api->getUserId();
+
+        try {
+            $layer->updateKML($userId, $bms);
+
+            return new JSONResponse(array(
+                "status" => "success"
+            ), Http::STATUS_OK);
+        } catch (BusinessLayerException $e) {
+            return new JSONResponse(array(
+                "status" => "error",
+                "message" => $e->getMessage()
+            ), Http::STATUS_INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
