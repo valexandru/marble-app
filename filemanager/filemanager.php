8df1c781e875643e492cc17e214ef670b4ca4bf2
Andrei Duma <andrei.duma.dorian@gmail.com>
2013-07-15 00:16:37 +0300

    Fixes #6: marble directories are now set up

--- filemanager/filemanager.php
+++ filemanager/filemanager.php
@@ -6,10 +6,12 @@ use \OC\Files\View;
 
 class FileManager {
 
-    public static function read($userId, $path) {
-        $view = new View('');
+    private static $view;
 
-        $contents = $view->file_get_contents($userId . '/marble/' . $path);
+    public static function read($userId, $path) {
+        self::init($userId);
+
+        $contents = self::$view->file_get_contents($userId . '/marble/' . $path);
         if (!$contents)
             throw new FileManagerException('Could not read from file \'' . $path . '\'');
 
@@ -17,27 +19,43 @@ class FileManager {
     }
 
     public static function write($userId, $path, $contents) {
-        $view = new View('');
-        if (!$r = $view->file_put_contents($userId . '/marble/' . $path, $contents))
+        self::init($userId);
+
+        if (!$r = self::$view->file_put_contents($userId . '/marble/' . $path, $contents))
             throw new FileManagerException('Could not write to file \'' . $path . '\'');
 
         return $r;
     }
 
     public static function delete($userId, $path) {
-        $view = new View('');
-        if (!$r = $view->unlink($userId . '/marble/' . $path))
+        self::init($userId);
+
+        if (!$r = self::$view->unlink($userId . '/marble/' . $path))
             throw new FileManagerException('Could not delete file \'' . $path . '\'');
 
         return $r;
     }
 
     public static function filemtime($userId, $path) {
-        $view = new View('');
-        if (!$mtime = $view->filemtime($userId . '/marble/' . $path))
+        self::init($userId);
+
+        if (!$mtime = self::$view->filemtime($userId . '/marble/' . $path))
             throw new FileManagerException('Couldn\'t get the timestamp for file \'' . $path . '\'');
 
         return $mtime;
+    }
+
+    public static function init($userId) {
+        self::$view = new View('');
+
+        if (!self::$view->is_dir($userId . '/marble')) {
+            if (!self::$view->mkdir($userId . '/marble') ||
+                !self::$view->mkdir($userId . '/marble/routes') ||
+                !self::$view->mkdir($userId . '/marble/bookmarks')) {
+                throw new FileManagerException('Couldn\'t setup Marble directories');
+            }
+        }
+        return true;
     }
 
 }
